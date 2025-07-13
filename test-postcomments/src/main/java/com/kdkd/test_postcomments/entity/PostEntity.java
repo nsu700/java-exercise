74aa60d56a5572a1a21bcbc5662d0246a4623fe1
Nick <nicksu383@gmail.com>
2025-07-13 14:58:26 +0800

    Setup default constructor for both entities
    Fix #8

--- test-postcomments/src/main/java/com/kdkd/test_postcomments/entity/PostEntity.java
+++ test-postcomments/src/main/java/com/kdkd/test_postcomments/entity/PostEntity.java
@@ -7,11 +7,15 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name="posts")
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class PostEntity {
   @Id
   @GeneratedValue(strategy=GenerationType.IDENTITY)
--- test-postcomments/src/main/java/com/kdkd/test_postcomments/entity/UserEntity.java
+++ test-postcomments/src/main/java/com/kdkd/test_postcomments/entity/UserEntity.java
@@ -5,11 +5,15 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name="users")
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserEntity {
   @Id
   @GeneratedValue(strategy=GenerationType.IDENTITY)
